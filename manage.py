e816cb8912426aa53fc4c8b328c02e185d9189b0
HafsaA85 <hafsaadeel4@gmail.com>
2026-03-09 13:17:18 +0000

    Fix project structure for deployment

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'empowering_muslimahs_mind.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resilia_project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
