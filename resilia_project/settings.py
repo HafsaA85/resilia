da3ef3568fcf5fe9dab2307ed287eff6b12d0a65
HafsaA85 <hafsaadeel4@gmail.com>
2026-04-04 00:38:21 +0100

    Debug

--- resilia_project/settings.py
+++ resilia_project/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-secret-key-for-dev-only")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "True") == "True"
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = os.getenv(
     "ALLOWED_HOSTS",
