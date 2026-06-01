d9c60bdabc0e1b8ba630f0f72ef606abe6b56176
HafsaA85 <hafsaadeel4@gmail.com>
2026-06-01 13:52:09 +0100

    forgot password templates and urls

--- resilia_project/settings.py
+++ resilia_project/settings.py
@@ -177,15 +177,17 @@ LOGIN_URL = "/login/"             # where @login_required sends users
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = "mail.zaynsolutions.com"
-EMAIL_PORT = 25
+EMAIL_HOST = "smtp-relay.brevo.com"
+EMAIL_PORT = 587
 EMAIL_USE_SSL = False
-EMAIL_USE_TLS = False
+EMAIL_USE_TLS =  True
+
+DEFAULT_FROM_EMAIL = "info@veylin.co.uk"
 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
 
 
